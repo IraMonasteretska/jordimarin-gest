950fa454044968542a1fbc90b105d82408d25ee4
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-08-01 16:39:55 +0300

    add first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+
+    // change header on scroll --------------------------- //
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.header').addClass('change-bg');
+        } else {
+            $('.header').removeClass('change-bg');
+        }
+    });
+
+    // burger ---------- //
+    var burger = document.querySelector(".burger2");
+    document.querySelector(".header__burger").addEventListener("click", function () {
+        return burger.classList.toggle("on");
+    });
+
+    $('.header__burger').click(function () {
+        $('.header__menu').toggleClass('show');
+    });
+
+
+});
