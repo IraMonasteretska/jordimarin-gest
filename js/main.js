7b8972f11f06abb4b1153adb77dd427ca3dd0b0d
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2023-08-04 10:32:31 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
     // change header on scroll --------------------------- //
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
+        if ($(this).scrollTop() > 100) {
             $('.header').addClass('change-bg');
         } else {
             $('.header').removeClass('change-bg');
@@ -18,6 +18,30 @@ $(document).ready(function () {
 
     $('.header__burger').click(function () {
         $('.header__menu').toggleClass('show');
+    });
+
+
+    // fancybox gallery
+    Fancybox.bind('[data-fancybox="gallery"]', { 
+        loop: false, // Включаємо зациклення галереї
+        thumbs: {
+            autoStart: true, // Включаємо автозапуск показу мініатюр
+            hideOnClose: false // Не ховати мініатюри після закриття галереї
+        },
+        // Дозволяємо максимальне збільшення зображення при зумі
+       
+        
+    });
+
+    Fancybox.bind('[data-fancybox]', {
+        loop: false,
+        toolbar: false,
+        contentClick: "iterateZoom",
+        Images: {
+            Panzoom: {
+                maxScale: 2,
+            },
+        },
     });
 
 
